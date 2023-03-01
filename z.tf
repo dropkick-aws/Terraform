0a91d182aa499bfdec0f5707bc21c4b6d02e0f49
dropkick-aws <126700005+dropkick-aws@users.noreply.github.com>
2023-03-01 09:28:41 -0800

    Update z.tf

--- z.tf
+++ z.tf
@@ -1,1 +1,37 @@
+// Define the AWS provider
+provider "aws" {
+  region = var.region
+}
+
+// Define the S3 bucket resource
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+}
+
+// Define the IAM user resource
+resource "aws_iam_user" "example_user" {
+  name = var.user_name
+}
+
+// Assign the user to the bucket
+resource "aws_s3_bucket_policy" "example_policy" {
+  bucket = aws_s3_bucket.example_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          AWS = aws_iam_user.example_user.arn
+        }
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = "${aws_s3_bucket.example_bucket.arn}/*"
+      }
+    ]
+  })
+}
 
